d0151be8d699aa9ea3d4f5d74ce6c2e2d11f0b2c
normahtds <125906525+normahtds@users.noreply.github.com>
2023-02-20 15:28:21 -0600

    se creo el método correr

--- personajes.py
+++ personajes.py
@@ -4,3 +4,11 @@ class Personaje:
     especie="Humano"
     nombre="MasterChief"
     altura=2.70
+    
+    #metodos
+    def correr(self, status):
+        if(status):
+            print("El personaje "+self.nombre +" esta corriendo")
+        else:
+            print("El personaje "+self.nombre +" se detuvo")
+                
