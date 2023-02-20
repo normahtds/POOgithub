8f44cbb5f8f3fc4afa4641a94d99b6a1a8740d81
normahtds <125906525+normahtds@users.noreply.github.com>
2023-02-20 15:44:23 -0600

    Creamos objetos e instancias

---
+++ objetos.py
@@ -0,0 +1,9 @@
+from personajes import *
+
+#Creamos una instancia Heroe de la clase Personaje 
+Heroe = Personaje()
+
+#usamos los atributos
+print("El personaje se llama " + Heroe.nombre)
+print("pertenece a la especie " + Heroe.especie)
+print("y una altura " + str(Heroe.altura))
