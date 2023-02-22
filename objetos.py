59f94c354b6c61e4115b62981b2fc1295f6a6ef1
normahtds <125906525+normahtds@users.noreply.github.com>
2023-02-22 15:33:32 -0600

    ejemplificación del constructor

--- objetos.py
+++ objetos.py
@@ -1,9 +1,42 @@
 from personajes import *
 
-#Creamos una instancia Heroe de la clase Personaje 
-Heroe = Personaje()
+#1. Solicitamos los datos para los objetos
+print("")
+print("### solicitud Datos Heroe ###")
+especieH=input("Escribe la especie del Heroe: ")
+nombreH=input("Escribe el nombre del Heroe: ")
+alturaH=input("Escribe la altura del Heroe: ")
+recargaH=int(input("Ingresa las balas para el Heroe: "))
 
-#usamos los atributos
+print("")
+print("### solicitud Datos Villano ###")
+especieV=input("Escribe la especie del Villano: ")
+nombreV=input("Escribe el nombre del Villano: ")
+alturaV=float(input("Escribe la altura del Villano: "))
+recargaV=int(input("Ingresa las balas para el Villano: "))
+
+#2. Creamos los objetos
+Heroe = Personaje(especieH,nombreH,alturaH)
+Villano = Personaje(especieV,nombreV,alturaV)
+
+
+#3. Usamos los atributos del Heroe y Villano
+print("")
+print("### atributos y metodos del Heroe ###")
 print("El personaje se llama " + Heroe.nombre)
 print("pertenece a la especie " + Heroe.especie)
 print("y una altura " + str(Heroe.altura))
+
+Heroe.correr(True)
+Heroe.lanzarGranadas()
+Heroe.recargarArma(recargaH)
+
+print("")
+print("### atributos y metodos del Villano ###")
+print("El personaje se llama " + Villano.nombre)
+print("pertenece a la especie " + Villano.especie)
+print("y una altura " + str(Villano.altura))
+
+Villano.correr(True)
+Villano.lanzarGranadas()
+Villano.recargarArma(recargaV)
--- personajes.py
+++ personajes.py
@@ -1,14 +1,22 @@
 class Personaje:
     
-    #atributos
-    especie="Humano"
-    nombre="MasterChief"
-    altura=2.70
+    #agregamos el costructor del personaje
+    def __init__(self,esp,nom,alt):
+        self.especie= esp
+        self.nombre= nom
+        self.altura= alt
     
     #metodos
     def correr(self, status):
         if(status):
-            print("El personaje "+self.nombre +" esta corriendo")
+            print("El personaje " + self.nombre + " esta corriendo")
         else:
-            print("El personaje "+self.nombre +" se detuvo")
+            print("El personaje " + self.nombre + " se detuvo")
+            
+    def lanzarGranadas(self):
+        print("El personaje " + self.nombre + " se lanzo una granada") 
                 
+    def  recargarArma(self, municiones):
+        cargardor=10
+        cargardor = cargardor + municiones
+        print("El arma recargada tiene " + str(cargardor) + " balas")
