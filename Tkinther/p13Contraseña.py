9ed4dac91bed447f85a65272c16359973a178a65
normahtds <125906525+normahtds@users.noreply.github.com>
2023-03-08 15:38:22 -0600

    creación de ventana de p13

---
+++ Tkinther/p13Contraseña.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import tkinter as tk
+
+
+
+ventana= Tk()
+ventana.title("Verificacion de contraseñas")
+ventana.geometry("500x250")
+
+seccion1= Frame(ventana)
+seccion1.pack(expand= True, fill= 'both', bg="#d8f7ba")
+
+titulo= Label(seccion1, text="Verificacion de contraseñas")
+
+var1= tk.StringVar()
+lblContra= Label(seccion1,text="Contraseña: ").pack()
+txtContra= Entry(seccion1, show="*",textvariable=var1).pack()
+
+botonComprobar= Button(seccion1,text="Comprobar")
+botonComprobar.pack()
+
+ventana.mainloop()
