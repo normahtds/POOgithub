914b5ee7a0fbcd21c7afad7605aa39d6ce714182
normahtds <125906525+normahtds@users.noreply.github.com>
2023-03-08 14:33:15 -0600

    soluciones del practica 12

---
+++ Tkinter/validacion.py
@@ -0,0 +1,9 @@
+from validacion import *
+from tkinter import Tk,Button,Frame,Label,Entry,messagebox
+
+def mostrarmensaje():
+    messagebox.showinfo("Informacion","Te informo que todo fallo con exito")
+    messagebox.showerror("Error","Perdon te falle!!")
+    print(messagebox.askokcancel("Pregunta","Seguro que quieres a guardar algo"))
+    print(messagebox.askyesnocancel("Pregunta2","Seguro que quieres a guardar algo"))
+    print(messagebox.askquestion("Pregunta2","Seguro que quieres a guardar algo"))
